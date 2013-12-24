56ed1e7a1f299de608085a4da5a672ea607fc901
susapa <>
2013-12-24 09:25:46 +0700

    test Abstract

---
+++ testPrint.php
@@ -0,0 +1,21 @@
+<?php
+$a = array('b'=>'bat','a'=>'ant','c'=>'cat');
+print_r($a);
+echo '<hr>';
+$b =array('bat','cat','dog');
+print_r ($b);
+echo '<hr>';
+$c =array('bat','cat','dog');
+foreach ($c as $arr)
+{
+    echo $arr."<br>";
+}
+foreach ($a as $ar1=>$ar2)
+{
+    echo "Key = ".$ar1."Value = ".$ar2.'<br>';
+}
+foreach ($b as $key=>$val)
+{
+    echo "Key = ".$key."Value = ".$val.'<br>';
+}
+?>
