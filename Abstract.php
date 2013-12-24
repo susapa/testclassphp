dd2b690e9f13856b1aa363d2ba2ef6af2af63c45
susapa <>
2013-12-24 09:28:00 +0700

    test Abstract

--- Abstract.php
+++ Abstract.php
@@ -1,11 +1,34 @@
 <?php
-class employee
+abstract  class AbstractClass
 {
-const  empname="";
-empname = $empname;
-          $this->empage = $empage;
-      }
- 
-      abstract function outputData();
-   }
+    abstract protected function  getValue();
+    abstract protected function  prefixValue();
+    
+    public function  printOut(){
+        echo $this->getValue()."\n";
+    }
+}
+class ConcreteClass1 extends AbstractClass
+{
+    protected function getValue() {
+        return "ConcreteClass1";
+    }
+
+    protected function prefixValue($prefix) {
+        return "{$prefix}ConcreteClass1";
+    }    
+}
+class ConcreteClass2 extends AbstractClass
+{
+    protected function getValue() {
+           return "ConcreteClass2";
+    }
+
+    protected function prefixValue($prefix) {
+        return "{$prefix}ConcreteClass2";
+    }    
+}
+$cass1 = new ConcreteClass1();
+$cass1->printOut();
+//echo $class1->prefixValue('FOO_')."\n";
 ?>
