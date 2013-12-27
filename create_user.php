668f543294585bba9a15c75cb53819b1f54149e5
susapa <>
2013-12-27 16:50:29 +0700

    Complete

---
+++ create_user.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head> 
+    <?php
+    include("/humanclass.php");
+    include("/connectDB_1.php");
+    ?>
+    <body>
+      <f
+        
+    </body>
+</html>
